b3632ed4f8eb3bb3de5f280c04dc74f1000316d3
lh99j <grand2181@gmail.com>
2022-01-11 20:40:43 +0900

    2022-01-11 - 월간 코드 챌린지 시즌3 - 없는 숫자 더하기 - Level 1

---
+++ Algorithm_Kotlin/src/nonnum_Plus.kt
@@ -0,0 +1,27 @@
+fun solution1(numbers:IntArray):Int{
+    var answer:Int = 0
+
+    var numberarray = arrayListOf(0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
+
+    var array = ArrayList<Int>()
+
+    for(i in 0 until numbers.size){
+        if(numberarray.contains(numbers[i])){
+            numberarray.remove(numbers[i])
+        }
+    }
+
+    for(i in numberarray){
+        answer += i
+    }
+
+    return answer
+}
+
+fun main(args:Array<String>){
+
+    val intarray = intArrayOf(1,2,3,4,6,7,8,0)
+    val result = solution1(intarray)
+
+    println(result)
+}
