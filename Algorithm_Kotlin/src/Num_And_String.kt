b2a7f6d2f297803f3f3fa92ed5fa0e0c7dce43dd
lh99j <grand2181@gmail.com>
2022-01-11 17:18:17 +0900

    2022-01-11 Algorithm - 2021 kakao Algorihm - Level 1

---
+++ Algorithm_Kotlin/src/Num_And_String.kt
@@ -0,0 +1,20 @@
+
+fun solution(s:String):Int{
+    var answer:Int = 0
+
+    var numSet = arrayOf("zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
+
+    var str = s
+    for(i in 0 until numSet.size){
+        str = str.replace(numSet[i], i.toString())
+    }
+
+    return str.toInt()
+}
+
+fun main(args:Array<String>){
+    val stnum:String = "one4seveneight"
+
+    val result = solution(stnum)
+    println(result)
+}
