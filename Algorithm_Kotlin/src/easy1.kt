321705c3801153748a9cbef34052e958ad221aa8
lh99j <grand2181@gmail.com>
2022-04-26 02:58:03 +0900

    easy A+B

--- Algorithm_Kotlin/src/easy1.kt
+++ Algorithm_Kotlin/src/easy1.kt
@@ -1,7 +1,15 @@
 fun main(args:Array<String>){
     val num = readLine()!!.toInt()
+    var array1 = ArrayList<Int>()
 
-    for(i in 1..9){
-        println("${num} * ${i} = ${num * i}")
+    for(i in 1..num){
+        var sum:Int = 0
+        var (a, b) = readLine()!!.split(" ").map { it.toInt() }
+        sum = a + b
+        array1.add(sum)
+    }
+
+    for(i in 0 until array1.size){
+        println(array1[i])
     }
 }
