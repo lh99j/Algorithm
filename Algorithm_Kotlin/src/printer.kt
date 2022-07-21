6ce4dfba07fdadfbb6a86eacec5b6a6f3a40cf4b
lh99j <grand2181@gmail.com>
2022-07-21 17:05:59 +0900

    프로그래머스 - 프린터 ( level 2 )

---
+++ Algorithm_Kotlin/src/printer.kt
@@ -0,0 +1,36 @@
+import java.util.*
+
+class printer {
+    fun solution(priorities: IntArray, location: Int): Int {
+        var keyqueue: Queue<Int> = LinkedList()
+        var valuequeue: Queue<Int> = LinkedList()
+        var answer = 0
+        var flag = 0
+
+        for (i in 0 until priorities.size) {
+            keyqueue.offer(i)
+            valuequeue.offer(priorities[i])
+        }
+
+        while (!(valuequeue.isEmpty())) {
+            var key = keyqueue.poll()
+            var value = valuequeue.poll()
+
+            if (valuequeue.any { value < it }) {
+                keyqueue.offer(key)
+                valuequeue.offer(value)
+            } else {
+                answer++
+                if (location == key)
+                    break
+            }
+
+        }
+        return answer
+    }
+}
+
+    fun main(args: Array<String>) {
+
+
+    }
