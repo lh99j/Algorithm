d8341a0e6ec3e488625c94331dd4a8584bb24bac
lh99j <grand2181@gmail.com>
2022-09-17 18:36:14 +0900

    프로그래머스 level2 - 최대 수식 ( 아직 다 못품 )

---
+++ Algorithm_Kotlin/src/programmers/kakao_max_susic.kt
@@ -0,0 +1,18 @@
+package programmers
+
+class kakao_max_susic {
+    fun max_susic() : Long{
+        var num : Long = 0
+        var str : String = ""
+        var susic_array : ArrayList<String> =ArrayList<String>()
+        var num_array : ArrayList<String> = ArrayList<String>()
+
+
+        return num
+    }
+}
+
+fun main(args:Array<String>){
+    val susic = kakao_max_susic()
+    susic.max_susic()
+}
